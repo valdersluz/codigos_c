f7e8be3cfe0aab9243cc0fa77e52255f1343df31
Valder Santos <valder@localhost.localdomain>
2024-05-18 18:53:38 -0300

    subindo códigos de treino correto

---
+++ prestacao/prestacao.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float valorMercadoria;
+    int prestacao, entrada;
+
+    // Solicita o valor da mercadoria
+    printf("Digite o valor da mercadoria: ");
+    scanf("%f", &valorMercadoria);
+
+    // Calcula o valor da prestação como sendo o maior inteiro possível
+    prestacao = (int) floor((valorMercadoria / 3));
+
+    // Calcula o valor da entrada
+    entrada = (int) (valorMercadoria - 2 * prestacao);
+
+    // Exibe os resultados
+    printf("Entrada: R$ %d,00\n", entrada);
+    printf("Prestacoes: 2 x R$ %d,00\n", prestacao);
+
+    return 0;
+}
